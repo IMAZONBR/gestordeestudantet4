05d89c61669d73b5a55e086a91be23006e63cd37
IMAZONBR <95544802+IMAZONBR@users.noreply.github.com>
2023-08-24 15:16:31 -0300

    main

---
+++ MEU_BD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Gestão_Estudantes_Kauã
+{
+    // A classe do nosso banco de dados.
+    internal class MEU_BD
+    {
+        private MySqlConnection conexao = new MySqlConnection("datasource=localhost; username=root; password=; database=sga_estudantes_bd_t4");
+
+        public MySqlConnection getConexao 
+        {
+            get
+            {
+                return conexao;
+            }
+        }
+
+        public void abrirConexao()
+        {
+            if (conexao.State == System.Data.ConnectionState.Closed)
+            {
+                conexao.Open();
+            }
+        }
+
+        public void fecharConexao()
+        {
+            if (conexao.State == System.Data.ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
